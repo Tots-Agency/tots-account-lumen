1b2313e48264f246a63f80909cbc35db71dcdc53
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-03 10:20:27 -0300

    Add account migration.

---
+++ database/migrations/2022_12_03_100000_create_tots_account_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tots_account', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->text('caption');
+            $table->tinyInteger('status')->nullable(false)->default(0)->comment('0 = Inactive, 1 = Active, 2 = Deleted');
+            $table->timestamps();
+
+            $table->index('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tots_account');
+    }
+};
