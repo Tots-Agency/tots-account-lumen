b192292e7f190a0dc43177165c3648299875e211
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-03 10:18:09 -0300

    Add account model.

---
+++ src/Models/TotsAccount.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of Model
+ *
+ * @author matiascamiletti
+ */
+class TotsAccount extends Model
+{
+    protected $table = 'tots_account';
+    
+    //protected $casts = ['data' => 'array'];
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    //public $timestamps = false;
+}
