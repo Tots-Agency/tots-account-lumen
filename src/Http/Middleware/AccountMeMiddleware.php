07949ece95f5d33897a144e4b6e318d42ed6d041
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-08 13:19:31 -0300

    Add account middleware.

---
+++ src/Http/Middleware/AccountMeMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tots\Account\Http\Middleware;
+
+use Closure;
+use Tots\Account\Models\TotsAccount;
+use Tots\Account\Models\TotsAccountPermission;
+
+class AccountMeMiddleware
+{
+    /**
+     * Obtiene la cuenta del middleware anterior y verifica si el usuario tiene permisos para esa cuenta.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        /** \Tots\Account\Models\TotsAccount $account */
+        $account = $request->input('account');
+
+        // Verify if has permission in account
+        $permission = TotsAccountPermission::where('account_id', $account->id)->where('user_id', $request->user()->id)->first();
+        if($permission === null){
+            throw new \Exception('Not has permission');
+        }
+
+        return $next($request->merge([TotsAccountPermission::class, $permission]));
+    }
+}
---
+++ src/Http/Middleware/AccountMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tots\Account\Http\Middleware;
+
+use Closure;
+use Tots\Account\Models\TotsAccount;
+
+class AccountMiddleware
+{
+    /**
+     * Obtiene la cuenta
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $accountId = $request->input('account_id') ?? $request->input('id');
+        if($accountId <= 0){
+            throw new \Exception('The account id is required');
+        }
+
+        $account = TotsAccount::where('id', $accountId)->first();
+        if($account === null){
+            throw new \Exception('The account not exist');
+        }
+
+        return $next($request->merge([TotsAccount::class => $account]));
+    }
+}
