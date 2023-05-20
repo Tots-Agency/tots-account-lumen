b34a435cace21d6e9080f97b3cf3a59124f3b1da
Matias Camiletti <matias.camiletti@gmail.com>
2023-05-20 16:09:34 -0300

    Add permission in create account.

--- src/Http/Controllers/CreateController.php
+++ src/Http/Controllers/CreateController.php
@@ -4,6 +4,7 @@ namespace Tots\Account\Http\Controllers;
 
 use Tots\Account\Models\TotsAccount;
 use Illuminate\Http\Request;
+use Tots\Account\Models\TotsAccountPermission;
 
 class CreateController extends \Laravel\Lumen\Routing\Controller
 {
@@ -12,16 +13,20 @@ class CreateController extends \Laravel\Lumen\Routing\Controller
         // Process validations
         $this->validate($request, [
             'title' => 'required|min:3',
-            'caption' => 'required|min:3',
         ]);
         // Create new model
         $item = new TotsAccount();
         $item->title = $request->input('title');
         $item->caption = $request->input('caption');
-        $item->status = $request->input('status');
-                
+        $item->status = TotsAccount::STATUS_ACTIVE;
         // Save new model
         $item->save();
+        // Create permission
+        $perm = new TotsAccountPermission();
+        $perm->account_id = $item->id;
+        $perm->user_id = $request->user()->id;
+        $perm->role = TotsAccountPermission::ROLE_ADMIN;
+        $perm->save();
         // Return data
         return $item;
     }
