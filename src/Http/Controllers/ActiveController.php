996a90810982a22bb26a85eff236b0822dbaa08c
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-17 10:24:26 -0300

    Add active and suspend controllers.

---
+++ src/Http/Controllers/ActiveController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tots\Account\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tots\Account\Models\TotsAccount;
+
+class ActiveController extends \Laravel\Lumen\Routing\Controller
+{
+    public function handle($id)
+    {
+        // Search user exist
+        $user = TotsAccount::where('id', $id)->first();
+        if($user === null){
+            throw new \Exception('This user not exist');
+        }
+        // Save new status
+        $user->status = TotsAccount::STATUS_ACTIVE;
+        $user->save();
+
+        return ['success' => true];
+    }
+}
